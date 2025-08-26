0287a147a8a3b198bc90e5d3b89f054337ff34ae
nikolay <n.smolov@yandex.ru>
2025-08-26 21:20:52 +0300

    adds raw config pkg

--- config/config.go
+++ config/config.go
@@ -1,1 +1,81 @@
 package config
+
+import (
+	"fmt"
+	"log/slog"
+	"os"
+	"strings"
+
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+)
+
+type brokerConfig struct {
+	SeedBrokers        []string `mapstructure:"seed_brokers"`
+	CARootCert         string   `mapstructure:"ca_root_cert"`
+	User               string   `mapstructure:"user"`
+	Pass               string   `mapstructure:"pass"`
+	SchemaRegistryURLs []string `mapstructure:"schema_registry_urls"`
+}
+
+type Config struct {
+	LogLevel slog.Level   `mapstructure:"log_level"`
+	Broker   brokerConfig `mapstructure:"broker"`
+}
+
+func Load() Config {
+	viper.SetConfigFile(getConfigFilepath())
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		die(err)
+	}
+
+	var cfg Config
+	err = viper.UnmarshalExact(&cfg)
+	if err != nil {
+		die(err)
+	}
+
+	print(cfg)
+
+	return cfg
+}
+
+func getConfigFilepath() string {
+	cmdLine := pflag.NewFlagSet(os.Args[0], pflag.ExitOnError)
+	arg := cmdLine.String("config", "/config.yaml", "config file")
+	_ = cmdLine.Parse(os.Args[1:])
+	env, ok := os.LookupEnv("ECOM_CONFIG_FILE")
+	if ok {
+		return env
+	}
+	return *arg
+}
+
+func die(err error) {
+	fmt.Printf("failed to load config file: %v\n", err)
+	os.Exit(2)
+}
+
+func print(c Config) {
+	tamplate := `
+	LogLevel=%q
+	SeedBrokers=%q
+	CARootCert=%q
+	User=%q
+	Pass=%q
+	SchemaRegistryURLs=%q
+
+`
+	fmt.Println("Loaded config:")
+	fmt.Printf(
+		strings.TrimLeft(tamplate, "\n"),
+		c.LogLevel,
+		c.Broker.SeedBrokers,
+		c.Broker.CARootCert,
+		c.Broker.User,
+		c.Broker.Pass,
+		c.Broker.SchemaRegistryURLs,
+	)
+}
--- go.mod
+++ go.mod
@@ -4,7 +4,22 @@ go 1.24.6
 
 require (
 	github.com/hamba/avro/v2 v2.29.0
+	github.com/spf13/pflag v1.0.6
 	github.com/stretchr/testify v1.11.0
+)
+
+require (
+	github.com/fsnotify/fsnotify v1.8.0 // indirect
+	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
+	github.com/sagikazarmark/locafero v0.7.0 // indirect
+	github.com/sourcegraph/conc v0.3.0 // indirect
+	github.com/spf13/afero v1.12.0 // indirect
+	github.com/spf13/cast v1.7.1 // indirect
+	github.com/subosito/gotenv v1.6.0 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.9.0 // indirect
+	golang.org/x/sys v0.29.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
 )
 
 require (
@@ -14,5 +29,6 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/spf13/viper v1.20.1
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
