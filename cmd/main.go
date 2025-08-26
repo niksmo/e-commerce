50588ba366628d643591b5d6b4cc8cf4da59d6f8
nikolay <n.smolov@yandex.ru>
2025-08-26 21:27:55 +0300

    adds init main cmd

--- cmd/main.go
+++ cmd/main.go
@@ -1,3 +1,37 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/niksmo/e-commerce/config"
+)
+
+func main() {
+	sigCtx, cancel := signalContext()
+	defer cancel()
+
+	appConfig := config.Load()
+
+	initLogger(appConfig.LogLevel)
+	slog.Info("application is running")
+
+	<-sigCtx.Done()
+	slog.Info("application is closed")
+}
+
+func signalContext() (context.Context, context.CancelFunc) {
+	return signal.NotifyContext(
+		context.Background(),
+		syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT,
+	)
+}
+
+func initLogger(level slog.Leveler) {
+	opts := &slog.HandlerOptions{Level: level}
+	logger := slog.New(slog.NewJSONHandler(os.Stderr, opts))
+	slog.SetDefault(logger)
+}
