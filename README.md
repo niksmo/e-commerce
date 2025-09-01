f3fa891e65989265c7cc2ebf4c37bb0f20def8c6
nikolay <n.smolov@yandex.ru>
2025-09-01 14:57:05 +0300

    adds create products migration

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 E-commerce
+
+```
+./topicmaker --config ./config.yaml
+```
+
+```
+./migrator --storage-path 'user:pass@127.0.0.1:5432/ecom?sslmode=disable' --migrations-path ./migrations
+```
+
+```
+./ecom --config ./config.yaml
+```
--- migrations/0_create_products_table.up.sql
+++ migrations/0_create_products_table.up.sql
@@ -1,5 +1,23 @@
 BEGIN;
 
--- MAGIC
+CREATE TABLE IF NOT EXISTS products (
+    id bigint GENERATED ALWAYS AS IDENTITY UNIQUE,
+    product_id varchar(100) NOT NULL,
+    name varchar(100) NOT NULL,
+    sku varchar(100) NOT NULL,
+    brand varchar(100) NOT NULL,
+    category varchar(100) NOT NULL,
+    description text NOT NULL,
+    price_amount DECIMAL(18,2) NOT NULL,
+    price_currency varchar(30) NOT NULL,
+    available_stock int NOT NULL,
+    tags varchar(30)[] NOT NULL,
+    images json NOT NULL,
+    specifications json NOT NULL,
+    store_id bigint NOT NULL,
+    PRIMARY KEY (product_id, store_id)
+);
+
+CREATE INDEX IF NOT EXISTS products_name_index ON products (name);
 
 COMMIT;
